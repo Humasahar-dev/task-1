174fc5aed0343e4deebb0475d40ced0401970a05
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-04-07 20:20:09 +0500

    Task1

---
+++ script/App.js
@@ -0,0 +1,117 @@
+
+
+
+    // DAY 1 (5 April):
+
+// alert("welcome sahar");
+
+// var user = prompt('Put Your ID');
+// alert("Successfully added")
+
+// var arr= [ 'hello' , 'unzyy'];
+// document.write(arr);
+// console.log(arr);
+
+// var field = "BS Economics";
+// document.write(field);
+//  console.log (field);
+
+//  var Course = "operating system";
+// document.write(Course);
+//  console.log (Course);
+
+//  var course_code = "abc_123";
+// document.write(course_code);
+//  console.log (course_code);
+
+// var num1 =+ prompt("Num 1 Please");
+// var num2 =+ prompt("Num 2 Please");
+// document.write(originalNumber);
+// console.log(num1+num2);
+
+
+// var originalNumber = 12 - 24;
+// document.write(originalNumber)
+// console.log(12-24)
+
+// var num = 20;
+// var originalNumber = num + 100;
+// document.write(originalNumber);
+// console.log(num + 100);
+
+// var num = 30;
+// var originalNumber = num + 100;
+// document.write(originalNumber);
+// console.log(num + 300);
+
+
+// var originalNumber = 5 * 10;
+// document.write(originalNumber);
+// console.log(4 * 10); 
+
+// var num = 11;
+// var NewNum = 1;
+// var originalNumber = num + NewNum;
+// document.write(originalNumber)
+// console.log(num + NewNum)
+
+// var num = 5;
+// var newNum = num++
+// document.write(newNum)
+// console.log(num ++)
+
+// var num = 6;
+// var newNum = ++num
+// document.write(newNum)
+// console.log(++num)
+
+// var num = 3;
+// var newNum = num--
+// document.write(newNum)
+// console.log(num--)
+
+// var num = 3;
+// var newNum = --num
+// document.write(newNum)
+// console.log(--num)
+
+// var TotalValue= 5+5*10 ;
+// var TotalValue = 5+ (5*10);
+// document.write(TotalValue)
+// console.log(5+ (5*10))
+
+
+// var TotalValue = (5+5) *10;
+// document.write(TotalValue)
+// console.log((5+5) *10)
+
+
+// totalValue = ((1* 2) * 3) + 4;
+// document.write(totalValue)
+// console.log(((1* 2) *3) + 4)
+
+
+// totalValue = (1* 2) * (4+ 5)
+// document.write(totalValue)
+// console.log((1* 2) * (2 + 4))
+
+
+// var message = "Thanks, ";
+// var userName = "sahar";
+// var banger = "!";
+// var value = message + userName + banger;
+// alert(value);
+
+// var question = "Your religion";
+// var defaultAnswer = "islam";
+// var spec = prompt(question, defaultAnswer);
+
+
+// var numberOfCourses = prompt("How many courses in your current semester?");
+// var tooManyCourses = numberOfCourses + 1;
+
+//JS TASKR [7 April]:
+var zakatPercentage = 0.025;
+var userInput = Number( prompt("Enter your total Amount"));
+var result = (zakatPercentage) * (userInput);
+alert ("Your zakat value is " + result)
