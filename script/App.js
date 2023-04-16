b24f9944996f8ed4b3b08e225792545b5dacaed7
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-04-16 17:40:34 +0500

    Task2

--- script/App.js
+++ script/App.js
@@ -110,8 +110,109 @@
 // var numberOfCourses = prompt("How many courses in your current semester?");
 // var tooManyCourses = numberOfCourses + 1;
 
-//JS TASKR [7 April]:
-var zakatPercentage = 0.025;
-var userInput = Number( prompt("Enter your total Amount"));
-var result = (zakatPercentage) * (userInput);
-alert ("Your zakat value is " + result)
+
+//              Task1
+// var zakatPercentage = 0.025;
+// var userInput = Number( prompt("Enter your total Amount"));
+// var result = (zakatPercentage) * (userInput);
+// alert ("Your zakat value is " + result)
+
+
+                //    Task 2
+// var family_members = Number(prompt("please enter the total number of your family members:"));
+// var select_method = prompt("please select  fitrah paid method:No = 1. Khajoor - Rs300 per person No= 2. Atta - Rs160 per person No = 3. Chawal - Rs 350 per person");
+
+//          
+// var price_ eachperson;
+// if (method === "1") {
+//   pricePerPerson = 300;
+// } else if (method === "2") {
+//   pricePerPerson = 160;
+// } else if (method === "3") {
+//   pricePerPerson = 350;
+// } else {
+//   alert("Fitrah amount For total family members.");
+// }
+
+// var fitrah_amount = price_eachperson * family_members;
+
+// alert("The  total fitrah amount  " + family_members + " family members using the " + method + " method is Rs" + fitrahAmount + ".");
+
+
+// Task3
+
+// var userFavNum =prompt("Enter your number");
+// var secretNum= (10 );
+
+// if( userFavNum>=secretNum) {
+//         console.log("Congratulation u won ");        
+
+//     }else{
+//         console.log("u lost");
+//     }
+   
+
+
+//                     Task 4
+//  var your_name  = prompt(" your good name");
+// // var capital_word = your_name[0].toUpperCase();
+// var new_name= your_name.slice(1).toLowerCase()
+// document.write(capital_word+new_name )
+
+
+
+//         Task5
+
+// var contactsNo = []
+// var ContactName =[]
+
+// for ( var i =0; i<3; i++ )
+// {
+//   var CNo = prompt ('Number')
+//   var CName = prompt ('Name')
+
+//   contactsNo.push(contactsNo)
+//   ContactName.push(ContactName)
+// }
+
+// console.log(contactsNo)
+// console.log(ContactName)
+
+//               Task 6
+// var product =['sooper', 'zeeraplus' ,'tuc',' prince', 'saltish' ,'lemonsandwitch ','choclatechip']
+// product.splice(2,2,"peanutpik","bisticis")
+// console.log( product)
+
+
+
+// Task 7
+
+//     var nationality = prompt("What is your nationality?");
+// var gender = prompt("What is your gender?");
+//  var age = Number(prompt("What is your age?"));
+
+
+// if (nationality === "Pakistani" || nationality === "Indian") {
+//   // Check gender and age
+//   if (gender === "male" && age >= 18) {
+//     console.log("You are eligible to vote.");
+//   } else if (gender === "female" && age >= 18) {
+//     console.log("You are eligible to vote.");
+//   } else if (age < 18) {
+//     console.log("You are not eligible to vote.");
+//   } else {
+//     console.log(" You are not eligible to vote.");
+//   }
+// } else {
+//   console.log(" You are not eligible to vote.");
+// }
+
+//                       task 8
+
+// var WorldCupteam = ["babar azam", "imad", "shaheen afridi", "hafeez", "wahab", "aamir", "shoaib", "imam", "asif", "hassan", "hassnain", "sarfaraz", "fakahr", "shadab", "haris sohail"];
+
+// var finalPlayers = WorldCupteam.slice(0,11); 
+// console.log( finalPlayers);
+ 
+
+
